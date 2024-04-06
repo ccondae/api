57afcf65faab0b9c690af13bac5f715feb9902d1
rookedsysc <rookedsysc36@gmail.com>
2024-04-06 23:45:13 +0900

    feat 특정 카테고리의 답변없는 쿼리 불러오는 API 개발

--- src/main/kotlin/org/ccondaeapi/infrastructure/repository/CategoryRepository.kt
+++ src/main/kotlin/org/ccondaeapi/infrastructure/repository/CategoryRepository.kt
@@ -1,6 +1,7 @@
 package org.ccondaeapi.infrastructure.repository
 
 import org.ccondaeapi.entity.Category
+import org.ccondaeapi.infrastructure.repository.ifs.QuestionRepositoryCustom
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface CategoryRepository : JpaRepository<Category, Long> {
--- src/main/kotlin/org/ccondaeapi/infrastructure/repository/QuestionRepository.kt
+++ src/main/kotlin/org/ccondaeapi/infrastructure/repository/QuestionRepository.kt
@@ -1,8 +1,8 @@
 package org.ccondaeapi.infrastructure.repository
 
 import org.ccondaeapi.entity.Question
+import org.ccondaeapi.infrastructure.repository.ifs.QuestionRepositoryCustom
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface QuestionRepository : JpaRepository<Question, Long>{
-
+interface QuestionRepository : JpaRepository<Question, Long> {
 }
