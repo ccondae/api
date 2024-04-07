54be9ffb9bc876750f555d717f6ec2269c25a6fe
rookedsysc <rookedsysc36@gmail.com>
2024-04-08 06:51:30 +0900

    remove 사용하지 않는 쿼리 제거

--- src/main/kotlin/org/ccondaeapi/infrastructure/repository/QuestionRepository.kt
+++ src/main/kotlin/org/ccondaeapi/infrastructure/repository/QuestionRepository.kt
@@ -5,8 +5,4 @@ import org.ccondaeapi.infrastructure.repository.ifs.QuestionRepositoryCustom
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface QuestionRepository : JpaRepository<Question, Long>, QuestionRepositoryCustom {
-    fun findByTitleContainingIgnoreCase(title: String): List<Question>
-    fun findByCodeContainingIgnoreCase(code: String): List<Question>
-    fun findByContentContainingIgnoreCase(content: String): List<Question>
-    fun findByPurposeContainingIgnoreCase(purpose: String): List<Question>
 }
